b309f6c221078e3a5c73a39b898e9d0a536c1205
Jorge Martinez <jorge.martinez09@uptc.edu.co>
2022-10-14 16:18:17 -0500

    Revert "Upload libraries"
    This reverts commit 82910c0bd6a66cc41d021b3b62cb38877e24cb3a.

--- src/co/edu/uptc/logic/WaitingZone.java
+++ src/co/edu/uptc/logic/WaitingZone.java
@@ -3,6 +3,10 @@ package co.edu.uptc.logic;
 import co.edu.uptc.SimpleList.SimpleList;
 import co.edu.uptc.entity.Client;
 
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 public class WaitingZone {
 
@@ -16,6 +20,7 @@ public class WaitingZone {
     //TODO create node with student and calculateAttentionTime
     public void addStudentZone(Client student){
         students.add(student);
+        student.setTime(LocalDate.now());
 
     }
     //TODO run list and compare actual hour with node.exit
